df9519f02ffb2687402d54003b6aaea9e5403e4b
jachub6 <37016060+jachub6@users.noreply.github.com>
2018-03-03 12:15:49 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/img', express.static(__dirname + '/img'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+});
+
+io.on('connection', function(socket) {
+  socket.on('vykreslit', function(data) {
+    io.emit("mouse", data);
+  });
+ 
+});
---
+++ static/game.js
@@ -0,0 +1,121 @@
+var socket = io();
+
+
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height =600;
+var context = canvas.getContext('2d');
+
+socket.on('mouse', function(data) {
+          context.strokeStyle = data.barva;
+          context.beginPath();
+          context.moveTo(data.exx,data.exy);
+          context.lineTo(data.x,data.y);
+          context.stroke();          
+});   
+
+/*var mouseDown = false;
+
+document.body.onmousedown = function() { 
+  mouseDown=true;
+}
+document.body.onmouseup = function() {
+  mouseDown=false;
+}
+
+function getMousePos(canvas, evt) {
+    if(mouseDown)
+    {
+      var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+    }
+           
+}    */
+
+var vykreslit = {
+            x: 0,
+            y: 0,
+            exx:0,
+            exy:0,
+            barva:"black"            
+};
+var isDrawing;
+canvas.onmousedown = function(e) {
+  isDrawing = true;
+  var rect = canvas.getBoundingClientRect();
+  var mx=e.clientX - rect.left;
+  var my=e.clientY - rect.top;
+  vykreslit.exx=mx;
+  vykreslit.exy=my;
+};
+canvas.onmousemove = function(e) {
+  if (isDrawing) {
+    var rect = canvas.getBoundingClientRect();
+    var mx=e.clientX - rect.left;
+    var my=e.clientY - rect.top;
+    vykreslit.x=mx;
+    vykreslit.y=my;
+    socket.emit("vykreslit", vykreslit);
+    vykreslit.exx=mx;
+    vykreslit.exy=my;
+
+  }
+};
+canvas.onmouseup = function() {
+  isDrawing = false;
+};
+
+canvas.addEventListener("touchstart", handleStart, false);
+canvas.addEventListener("touchend", handleEnd, false);
+canvas.addEventListener("touchmove", handleMove, false);
+var touchX,touchY;
+function getTouchPos(e) {
+        if (!e)
+            var e = event;
+
+        if (e.touches) {
+            if (e.touches.length == 1) { // Only deal with one finger
+                var touch = e.touches[0]; // Get the information for finger #1
+                touchX=touch.pageX-touch.target.offsetLeft;
+                touchY=touch.pageY-touch.target.offsetTop;
+            }
+        }
+    }
+
+function handleStart(e){
+  isDrawing = true;
+  getTouchPos(e);
+  var mx=touchX;
+  var my=touchY;
+  vykreslit.exx=touchX;
+  vykreslit.exy=touchY;
+  e.preventDefault();
+}
+
+function handleMove(e){
+    if (isDrawing) {
+    getTouchPos(e);
+    var mx=touchX;
+    var my=touchY;
+    vykreslit.x=mx;
+    vykreslit.y=my;
+    socket.emit("vykreslit", vykreslit);
+    vykreslit.exx=mx;
+    vykreslit.exy=my;
+    e.preventDefault();
+
+  }
+}
+
+function handleEnd(e){
+  isDrawing = false;
+  e.preventDefault();
+}
+
+function zmena()
+{
+  vykreslit.barva="red";
+}
