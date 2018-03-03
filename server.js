27b2004b523319a8c431072d9f358b9d2c1ab600
jachub6 <37016060+jachub6@users.noreply.github.com>
2018-03-03 12:38:20 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.set('port', 5000);
+app.set('port', 8080);
 app.use('/static', express.static(__dirname + '/static'));
 app.use('/img', express.static(__dirname + '/img'));
 
@@ -18,7 +18,7 @@ app.get('/', function(request, response) {
 });
 
 // Starts the server.
-server.listen(5000, function() {
+server.listen(8080, function() {
   console.log('Starting server on port 5000');
 });
 
